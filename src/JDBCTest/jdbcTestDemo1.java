bb5cb3f3d176a0c6805f5859268eddba7f5509c1
qinjiajun <505567352@qq.com>
2019-09-03 22:38:13 +0800

    refactor：change to use try-catch-resoures

--- src/JDBCTest/jdbcTestDemo1.java
+++ src/JDBCTest/jdbcTestDemo1.java
@@ -20,28 +20,29 @@ public class jdbcTestDemo1 {
 
 	public static void selectALL() {
 		//不放在try语句外面作用域无法作用与finally
-		Connection con = null;
-		Statement st = null;
-		ResultSet result = null;  
+//		Connection con = null;
+//		Statement st = null;
+//		ResultSet result = null;  
 		//导入jar包后注册驱动,使用什么驱动连接数据库
-		try {
-			//后面不带.class
-			Class.forName("com.mysql.jdbc.Driver");
-			
 		//url为固定写法  此处填写的是本机本地地址
 			String url = "jdbc:mysql://localhost:3306/web01";
 			//指定该url使用utf8编码
 //			String url = "jdbc:mysql://localhost:3306/web01?useUnicode=true&characterEncoding=UTF8";
 			String user = "root";
 			String password = "123456";
+		try(Connection con = DriverManager.getConnection(url, user, password);Statement st =con.createStatement();ResultSet result = st.executeQuery("select*from user") ) {
+			//后面不带.class
+			Class.forName("com.mysql.jdbc.Driver");
+			
+		
 			
 		//与数据库建立连接
-		 con =  DriverManager.getConnection(url, user, password);
+//		 con =  DriverManager.getConnection(url, user, password);
 		
 		//发起请求
-		 st = con.createStatement();
+//		 st = con.createStatement();
 		//查询语句
-		 result = st.executeQuery("select * from user");
+//		 result = st.executeQuery("select * from user");
 		while(result.next())
 		{
 			//逐行来打印id 名字 密码
@@ -54,50 +55,55 @@ public class jdbcTestDemo1 {
 		} catch (Exception e) {
 			System.out.println("注册失败，找不到该驱动");
 			e.printStackTrace();
-		} finally {
-			//使用该种做法避免前面出错后面不能执行
-			//先关闭语句再关闭连接
-		
-			try {
-				if(result!=null)
-				result.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
-		
-			try {
-				if(st!=null)
-				result.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
-		
-			try {
-				if(con!=null)
-				con.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
+//		} finally {
+//			//使用该种做法避免前面出错后面不能执行
+//			//先关闭语句再关闭连接
+//		
+//			try {
+//				if(result!=null)
+//				result.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		
+//			try {
+//				if(st!=null)
+//				result.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		
+//			try {
+//				if(con!=null)
+//				con.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
 		}
 	}
 	
 	public static boolean selectByUsernamePassword(String username,String password)
 	{
-		Connection con = null;
-		Statement st = null;
-		ResultSet result = null;
-		try {
+		String url = "jdbc:mysql://localhost:3306/web01";
+		String sql = "select * from user where username='"+username+"'and password = '"+password+"'";
+//		Connection con = null;
+//		
+//		ResultSet result = null;
+		//使用try-catch-resources 这一java 7引入的语法糖
+		/*
+		 * 既最大化简化了资源需手动释放的代码，其在Java 9中进行了优化（ 在 Java 7 中可以实现资源的自动关闭，但是要求必须将需要自动关闭的所有资源必须在try子句中进行初始化,否则编译不通过，*/
+		try(Connection con = DriverManager.getConnection(url, "root", "123456");Statement st = con.createStatement();ResultSet result = st.executeQuery(sql)) {
 			//加载驱动
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/web01";
-			 con = DriverManager.getConnection(url, "root", "123456");
-			 st = con.createStatement();
-			String sql = "select * from user where username='"+username+"'and password = '"+password+"'";
-			 result = st.executeQuery(sql);
-			 System.out.println(sql);
+			
+//			 con = DriverManager.getConnection(url, "root", "123456");
+//			 st = con.createStatement();
+			
+//			 result = st.executeQuery(sql);
+		 System.out.println(sql);
 			if(result.next())
 			{
 				return true;
@@ -107,30 +113,30 @@ public class jdbcTestDemo1 {
 		} catch (Exception e) {
 			
 			e.printStackTrace();
-		}finally {
-			try {
-				if(result!=null)
-				result.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
-		
-			try {
-				if(st!=null)
-				result.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
-		
-			try {
-				if(con!=null)
-				con.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
+//		}finally {
+//			try {
+//				if(result!=null)
+//				result.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		
+//			try {
+//				if(st!=null)
+//				result.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		
+//			try {
+//				if(con!=null)
+//				con.close();
+//			} catch (SQLException e) {
+//				
+//				e.printStackTrace();
+//			}
 			
 		}
 		return false;
